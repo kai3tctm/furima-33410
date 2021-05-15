911966b6e17ed61d0d38cdf1b9748a5ce4fcac76
kai3tctm <get_wild10run@yahoo.co.jp>
2021-05-15 20:30:06 +0900

    商品出品機能1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,27 +29,27 @@ RSpec.describe Item, type: :model do
         expect(@items.errors.full_messages).to include("Product info can't be blank")
       end
       it "カテゴリーが--のままだと登録できない" do
-        @items.category_id = '1'
+        @items.category_id = 1
         @items.valid?
         expect(@items.errors.full_messages).to include("Category -- 以外の項目を選択して下さい")
       end
       it "商品の状態が--のままだと登録できない" do
-        @items.product_status_id = '1'
+        @items.product_status_id = 1
         @items.valid?
         expect(@items.errors.full_messages).to include("Product status -- 以外の項目を選択して下さい")
       end
       it "配送料の負担が--のままだと登録できない" do
-        @items.delivery_burden_id = '1'
+        @items.delivery_burden_id = 1
         @items.valid?
         expect(@items.errors.full_messages).to include("Delivery burden -- 以外の項目を選択して下さい")
       end
       it "発送元の地域が--のままだと登録できない" do
-        @items.delivery_area_id = '1'
+        @items.delivery_area_id = 1
         @items.valid?
         expect(@items.errors.full_messages).to include("Delivery area -- 以外の項目を選択して下さい")
       end
       it "発送までの日数が--のままだと登録できない" do
-        @items.estimated_delivery_id = '1'
+        @items.estimated_delivery_id = 1
         @items.valid?
         expect(@items.errors.full_messages).to include("Estimated delivery -- 以外の項目を選択して下さい")
       end
@@ -63,8 +63,13 @@ RSpec.describe Item, type: :model do
         @items.valid?
         expect(@items.errors.full_messages).to include("Price is invalid. Input half-width characters.", "Price 半角数字で金額を入力して下さい（¥300〜9,999,999まで）")
       end
-      it "販売価格が¥300〜9,999,999以外の数字で入力されていると登録できない" do
-        @items.price = '200'
+      it "販売価格が¥300以下の数字で入力されていると登録できない" do
+        @items.price = 200
+        @items.valid?
+        expect(@items.errors.full_messages).to include("Price 半角数字で金額を入力して下さい（¥300〜9,999,999まで）")
+      end
+      it "販売価格が¥9,999,999以上の数字で入力されていると登録できない" do
+        @items.price = 10000000
         @items.valid?
         expect(@items.errors.full_messages).to include("Price 半角数字で金額を入力して下さい（¥300〜9,999,999まで）")
       end
@@ -72,6 +77,11 @@ RSpec.describe Item, type: :model do
         @items.image = nil
         @items.valid?
         expect(@items.errors.full_messages).to include("Image can't be blank")
+      end
+      it "商品に紐づくユーザーがいない場合登録できない" do
+        @items.user = nil
+        @items.valid?
+        expect(@items.errors.full_messages).to include("User must exist")
       end
     end
   end
